26861012fe0d647a72866cd0873ec037e17e8285
Gustavo Campos <gus.campos042@gmail.com>
2025-07-31 14:17:17 -0300

    cria timer

---
+++ include/Timer.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Timer {
+
+    using Clock = std::chrono::steady_clock;
+    using Marca = Clock::time_point;
+
+    string titulo;
+    Marca inicio;
+    vector<pair<string, Marca>> marcas;
+
+    float delta(Marca marca) {
+        return chrono::duration<double>(marca - inicio).count();
+    }
+
+public:
+
+    Timer(string titulo) {
+        this->titulo = titulo; 
+        this->inicio = Clock::now();
+    }
+
+    void marcar(string label) {
+        Marca marca = Clock::now();
+        marcas.emplace_back(label, marca);
+    }
+
+    void imprimir_resultado() {
+        
+        cout << "==" << titulo << "==";
+        for (auto [label, marca] : marcas)
+            cout << label << ": " << delta(marca) << "s\n";
+    }
+};
